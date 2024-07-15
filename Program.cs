aba41255e6c07b09ceb3278532ac827ce18f68ac
Jessica Sturgell <sturgell01@gmail.com>
2024-07-15 13:17:07 -0400

    added magic moose functionality

--- Program.cs
+++ Program.cs
@@ -17,6 +17,53 @@ void Main()
     MooseQuestion("Are you enthusiastic?", "Yay!", "You should try it!");
     MooseQuestion("Do you love C# yet?", "Good job sucking up to your instructor!", "You will...oh, yes, you will...");
     MooseQuestion("Do you want to know a secret?", "ME TOO!!!! I love secrets...tell me one!", "Oh, no...secrets are the best, I love to share them!");
+
+    // Magic Moose Intro
+    MooseSays("Thank you for answering my questions! Feel free to ask me something in return.");
+    MagicMoose();
+}
+
+
+void MagicMoose()
+{
+    List<string> answers = new List<string>()
+    { 
+        "As I see it, yes.",
+        "Ask again later.",
+        "Better not tell you now.",
+        "Cannot predict now.",
+        "Concentrate and ask again.",
+        "Do not count on it.",
+        "It is certain.",
+        "It is decidedly so.",
+        "Most likely.",
+        "My reply is no.",
+        "My sources say no.",
+        "Outlook not so good.",
+        "Outlook good.",
+        "Reply hazy, try again.",
+        "Signs point to yes.",
+        "Very doubtful.",
+        "Without a doubt.",
+        "Yes.",
+        "Yes, definitely.",
+        "You may rely on it."
+    };
+
+    string question = "";
+    do
+    {
+        Console.WriteLine("Ask me a question (or press Enter to quit): ");
+        question = Console.ReadLine();
+
+        if (question != "")
+        {
+            Random r = new Random();
+            int genRand = r.Next(answers.Count);
+            string answer = answers[genRand];
+            MooseSays(answer);
+        }
+    } while (question != "");
 }
 
 void MooseQuestion(string question, string yesResponse, string noResponse)
